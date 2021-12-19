3a0774d90c8cca91e317bb90834cc397a7c2a63d
osamayousuf90 <94833503+osamayousuf90@users.noreply.github.com>
2021-12-19 16:45:38 +0500

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,14 @@
+/*Hamburger For Menu */
+
+function myFunction() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
+
+  
+  
+
